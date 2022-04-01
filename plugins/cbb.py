d8cb3dcd0383c0bcdbf5e7f41b107f19b8856fdf
Ranju2004 <102154261+Ranju2004@users.noreply.github.com>
2022-04-01 10:42:12 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/Filmy_Hangama'>Click here</a>\n○ Channel : @Filmy_Hangama\n○ Support Group : @LegendsRequest</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/FHM_Movies'>Click here</a>\n○ Channel : @FHM_Movies\n○ Support Group : https://t.me/Movie_requests_kannada</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
